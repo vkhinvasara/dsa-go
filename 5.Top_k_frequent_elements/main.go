c65d2a9e924fb5b0c299f73e3c01d198729a9a67
vkhinvasara <vaibhavkhinvasara@gmail.com>
2025-02-19 03:07:40 +0530

    Implement function to find top K frequent elements in an array

--- 5.Top_k_frequent_elements/main.go
+++ 5.Top_k_frequent_elements/main.go
@@ -8,5 +8,25 @@ func main(){
 }
 
 func topKFrequent(nums []int, k int) []int{
-	
+	count := make(map[int]int);
+	freq := make([][]int, len(nums)+1);
+
+	for _, n := range nums{
+		count[n]++;
+	}
+	for i, v := range count{
+		freq[v]= append(freq[v], i);
+	}
+
+	var res []int;
+
+	for i:=len(freq)-1; i>=0; i--{
+		for _, n := range freq[i]{
+			res = append(res, n);
+			if len(res) == k{
+				return res;
+			}
+		}
+	}
+	return []int{};
 }
